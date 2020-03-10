61b96172eddaa73c386719a043382168b33b292b
unknown <maximecheniour@gmail.com>
2020-03-10 14:54:41 +0100

    added first exercises scripts

---
+++ scripts/exercises.py
@@ -0,0 +1,23 @@
+import csv
+import json
+import re
+import random as ran
+
+with open('sentences.csv', mode="r", encoding="utf8") as sen:
+    csv_reader = csv.reader(sen, delimiter='	')
+    lcnt = 0
+    y=[]
+    W = input("Choose the word you want to work with.\n")
+    for row in csv_reader:
+        lang = row[1]
+        text = row[2].lower()
+        if lang == 'eng' and bool(re.search( "(?:^|\W)"+W+"(?=$|\W)", text)):
+            x = text.replace(W,"_____")
+            y.append(x)
+            print(x)
+        lcnt += 1
+        if lcnt == 5000:
+            break
+    with open("jsone/" + W +'.json', 'w') as f:
+        f.write(json.dumps(y, indent=4))
+    print(f'Processed {lcnt} lines.')
